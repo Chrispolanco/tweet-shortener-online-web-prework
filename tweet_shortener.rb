69eb3b6f10adc2760745f0f7aa4f424746499609
Christian Polanco <chrispolanco@sbcglobal.net>
2019-06-03 02:40:09 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,29 +1,15 @@
-def word_substituter(tweet)
-  tweet.split.collect do |word|
-    if dictionary.keys.include?(word.downcase)
-      word = dictionary[word.downcase]
-    else
-      word
-    end
-  end.join(" ")
-end
-
-
-
-def bulk_tweet_shortener(tweets)
-  tweets.each do |tweet|
-    puts word_substituter(tweet)
-  end
-end
-
-
-
-def selective_tweet_shortener(tweet)
-  if tweet.length > 140
-    word_substituter(tweet)
-  else
-    tweet
-  end
+def dictionary
+dictionary = {
+  "too" => "2",
+  "to" => "2",
+  "two" =>"2",
+  "four" => "4",
+  "for" => "4",
+  "be" => "b",
+  "you" => "u",
+  "at" => "@",
+  "and" => "&"
+}
 end
 
 
